cc1ac7f3d6ba3798977c0ed7911859b12997ecf1
AnasBawazir <a.n.a.s.30@hotmail.com>
2020-07-01 06:53:02 +0300

    edit html source to contain all elements

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title> Robot Guide Map </title>
+    <link rel="stylesheet" type="text/css" href="assets/homeless.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+<body class="body">
+<script src="assets/connector.js"></script>
+
+<div class="container pt-3">
+    <section>
+        <img src="https://s-m.com.sa/images/logo.png" alt="no image">
+        <h3>Guide Map</h3>
+    </section>
+    <section>
+        <h6>Distance </h6>
+        <input id="distance" type="number" placeholder="distance in meter" min="1" pattern="[0-9]" required >
+    </section>
+    <h6>Direction </h6>
+
+    <div class="middle">
+
+        <section>
+            <button class="button button1"  name="direction" value="forward" onclick="doMove(this.value)"  ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z"/></svg></button>
+
+        </section>
+        <section>
+            <button class="button button2"  name="direction" value="left" onclick="doMove(this.value)"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z"/></svg></button>
+            <button class="button button4" name="direction" value="right" onclick="doMove(this.value)"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"  viewBox="0 0 24 24"><path d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z"/></svg></button>
+        </section>
+
+        <div class="move"> Map view </div>
+
+        <div class="move" id="txtMove"> </div>
+    </div>
+
+    <script>
+        function doMove(direction) {
+            distance = document.getElementById("distance").value;
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "assets/doMovement.php", true);
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhr.send("direction="+direction+"&"+"distance="+distance);
+            xhr.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("txtMove").innerHTML = this.responseText;
+                }
+            };
+        }
+    </script>
+
+
+    <div class="footer">
+        <p>By Anas Bawazir © </p>
+    </div>
+</div>
+
+</body>
+</html>
